0ee731beb55155dcda39b4b8754b3d874ff21b38
Dmitry Astapov <dastapov@gmail.com>
2011-08-13 15:52:07 +0100

    maxThreads -> numThreads, shorter lines

--- code/multi-threaded.hs
+++ code/multi-threaded.hs
@@ -8,14 +8,14 @@ import Trapezoid
 
 main :: IO ()
 main = do
-  let maxThreads = numCapabilities
+  let numThreads = numCapabilities
   aStr:bStr:nStr:_ <- getArgs
   let [a,b] = map read [aStr,bStr]
       n = read nStr
       h = (b - a) / fromIntegral n
-      localN = n `div` fromIntegral maxThreads
+      localN = n `div` fromIntegral numThreads
       chunks = parMap rseq (\threadNo ->
-         let localA = a + fromIntegral threadNo * fromIntegral localN * h
+         let localA = a + fromIntegral (threadNo * localN) * h
              localB = localA + fromIntegral localN * h
-             in trapezoid f localA localB localN h) [0..maxThreads-1]
+             in trapezoid f localA localB localN h) [0..numThreads-1]
   print (sum chunks)
