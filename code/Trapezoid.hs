8750e0b3cf9ef4107047211b4714d713a4582a9d
Dmitry Astapov <dastapov@gmail.com>
2011-08-13 15:23:00 +0100

    Sum is back into Trapezoid.hs, updated listings in article

--- code/Trapezoid.hs
+++ code/Trapezoid.hs
@@ -1,16 +1,15 @@
 module Trapezoid (trapezoid, f) where
 
-import Data.List
-
--- Integrate 'f' on interval [a,b] using 'n' steps of size 'h'
-trapezoid :: (Double -> Double) -> Double -> Double -> Int -> Double -> Double
+trapezoid :: (Double -> Double)  -- Function to integrate
+             -> Double -> Double -- integration bounds
+             -> Int              -- number trapezoids
+             -> Double           -- width of a trapezoid
+             -> Double
 trapezoid f a b n h =
-  h * foldl' (+) 0 (endPoints:internals)
+  h * sum (endPoints:internals)
   where
   endPoints = (f a + f b) / 2
   internals = map f $ take (n - 1) $ iterate (+h) (a + h)
 
 f :: Double -> Double
 f x = 4 / (1 + x * x)
--- f = cos
--- f = const 1
