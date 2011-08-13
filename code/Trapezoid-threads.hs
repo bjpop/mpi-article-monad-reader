b06fac286743fe9a1b813dcff173e1236b86a294
Dmitry Astapov <dastapov@gmail.com>
2011-08-13 11:12:34 +0100

    Brought code up to speed with ghc 7.x

--- code/Trapezoid-threads.hs
+++ code/Trapezoid-threads.hs
@@ -1,21 +1,20 @@
 module Main where
 
 import Control.Concurrent
+import GHC.Conc
 import Control.Parallel.Strategies
 import System (getArgs)
 import Data.List
 
 main :: IO ()
 main = do
-  -- I tried this on ghc 6.10.4, which has no getNumCapabilities
-  -- uncomment with later ghc's
-  maxThreads <- return 4 -- getNumCapabilities
+  let maxThreads = numCapabilities
   aStr:bStr:nStr:_ <- getArgs
   let [a,b] = map read [aStr,bStr]
       n = read nStr
       h = (b - a) / fromIntegral n
       localN = n `div` fromIntegral maxThreads
-      chunks = parMap rwhnf (\threadNo ->
+      chunks = parMap rseq (\threadNo ->
          let localA = a + fromIntegral threadNo * fromIntegral localN * h
              localB = localA + fromIntegral localN * h
              in trapezoid f localA localB localN h) [0..maxThreads-1]
