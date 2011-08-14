7b5a5e8a2861c43e2fc98bc25262e990bff34830
Bernie Pope <florbitous@gmail.com>
2011-08-14 19:14:27 +1000

    Changed trapezoid function to use explicit loop, generates faster code

--- code/Trapezoid.hs
+++ code/Trapezoid.hs
@@ -1,5 +1,21 @@
 module Trapezoid (trapezoid, f) where
 
+trapezoid :: (Double -> Double)
+             -> Double -> Double
+             -> Int
+             -> Double
+             -> Double
+trapezoid f a b n h =
+   h * (endPoints + internals)
+   where
+   endPoints = (f a + f b) / 2.0
+   internals = worker 1 (a + h) 0.0
+   worker :: Int -> Double -> Double -> Double
+   worker count x acc
+      | count == (n - 2) = acc
+      | otherwise = worker (count + 1) (x + h) (acc + f x)
+
+{-
 trapezoid :: (Double -> Double)  -- Function to integrate
              -> Double -> Double -- integration bounds
              -> Int              -- number trapezoids
@@ -10,6 +26,8 @@ trapezoid f a b n h =
   where
   endPoints = (f a + f b) / 2
   internals = map f $ take (n - 1) $ iterate (+h) (a + h)
+-}
 
 f :: Double -> Double
 f x = 4 / (1 + x * x)
+
