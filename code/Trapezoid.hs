0f2fd2fa7318485f960b54bf3ac423b1baa8433c
Dmitry Astapov <dastapov@gmail.com>
2011-08-14 12:16:28 +0100

    Removing commented out code

--- code/Trapezoid.hs
+++ code/Trapezoid.hs
@@ -15,19 +15,6 @@ trapezoid f a b n h =
       | count == (n - 2) = acc
       | otherwise = worker (count + 1) (x + h) (acc + f x)
 
-{-
-trapezoid :: (Double -> Double)  -- Function to integrate
-             -> Double -> Double -- integration bounds
-             -> Int              -- number trapezoids
-             -> Double           -- width of a trapezoid
-             -> Double
-trapezoid f a b n h =
-  h * sum (endPoints:internals)
-  where
-  endPoints = (f a + f b) / 2
-  internals = map f $ take (n - 1) $ iterate (+h) (a + h)
--}
-
 f :: Double -> Double
 f x = 4 / (1 + x * x)
 
